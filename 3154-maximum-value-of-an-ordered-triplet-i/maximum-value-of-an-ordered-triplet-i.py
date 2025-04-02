0c7b1a7300f2eef4803a24bc1845b3d68e7ab7d3
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-04-01 19:24:17 -0600

    Time: 3 ms (78.55%) | Memory: 17.9 MB (37.16%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         n = len(nums)
-        max_val, lmax, rmax = 0, 0, 0
+        max_val, three_max, two_max = 0, 0, 0
         for k in range(n):
-            max_val = max(max_val, rmax * nums[k])
-            rmax = max(rmax, lmax - nums[k])
-            lmax = max(lmax, nums[k])
+            max_val = max(max_val, two_max * nums[k])
+            two_max = max(two_max, three_max - nums[k])
+            three_max = max(three_max, nums[k])
         return max_val
