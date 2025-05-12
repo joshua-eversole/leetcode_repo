fc9ce0ec5feff0b5ff0ca4da27843b9a0d4c3fa1
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-05-12 14:44:23 -0600

    Time: 6 ms (82.91%) | Memory: 19.7 MB (22.51%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,17 +1,14 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # Init variables
-        result = [0] * len(nums)
-        l, r = 0, len(nums) - 1
-
-        # Run backwards through the array, so we start with the highest value and decrease
-        for i in range(len(nums) - 1, -1, -1):
-            # Take the bigger one, add it to the end, and move that pointer in one
+        n = len(nums)
+        squares = [0] * n
+        l, r = 0, n - 1
+        for i in range(n - 1, -1, -1):
             if abs(nums[l]) > abs(nums[r]):
-                result[i] = nums[l] ** 2
+                squares[i] = nums[l]**2
                 l += 1
             else:
-                result[i] = nums[r] ** 2
+                squares[i] = nums[r]**2
                 r -= 1
         
-        return result
+        return squares
