d188a1bebe7b18eb654909c008a8a0c9827dab98
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-06-19 15:11:00 +0800

    Time: 294 ms (15.80%) | Memory: 29.2 MB (6.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,36 +5,47 @@ class Solution:
         islands = 0
         directions = [(0,1), (1,0), (0,-1), (-1,0)]
 
-        def inBounds(row, col):
-            return 0 <= row < rows and 0 <= col < cols
-
-        def dfs(row, col):
+        def isLand(row, col):
             # Base case: if we've visited it already
             if (row, col) in visited:
-                return
+                return False
             
-
-            visited.add((row, col))
+            if not (0 <= row < rows and 0 <= col < cols):
+                return False
             
             # Base case: if we're not on land
-            if not inBounds(row, col) or grid[row][col] == "0":
-                return
-            
-            
-            # If this is land, add it to visited and check the neighbors
-            for move_row, move_col in directions:
-                dfs(row + move_row, col + move_col)
+            if  grid[row][col] == "0":
+                return False
 
-            return
+            visited.add((row, col))
+            return True
+        def dfs(row, col):
+            if isLand(row, col):
+                for move_row, move_col in directions:
+                    dfs(row + move_row, col + move_col)
+
+
+        def bfs(start_row, start_col):
+            q = deque()
+            q.append((start_row, start_col))
+            while q:
+                row, col = q.popleft()
+                if isLand(row, col):
+                    for move_row, move_col in directions:
+                        q.append((row + move_row, col + move_col))
+
         
+        #dfs
         for row in range(rows):
             for col in range(cols):
                 # Every time we find a new un(explored island, explore it and add one to the count
                 if grid[row][col] == "1" and (row, col) not in visited:
                     islands += 1
-                    dfs(row, col)
-        
+                    bfs(row, col)
         return islands
+
+
+        
             
 
         
