a77a7e1e1a96a51dc1681ee416266a30bb540dfe
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-06-19 13:52:29 +0800

    Time: 52 ms (6.52%) | Memory: 18.1 MB (56.84%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -8,28 +8,27 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def __init__(self):
-        self.visited = {} #key is original node, value is new node
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # Base case: if the node deosn't exist
-        if not node:
-            return
+        visited = {}
         
-        # Base case: if we've already visited the node
-        if node in self.visited:
-            return self.visited[node]
+        def dfs(node):
+            if not node:
+                return
+            
+            if node in visited:
+                return visited[node]
+            
+            # If we've reached here, we need to create this node
+            new_node = Node(node.val)
+            visited[node] = new_node
+            
+            for neighbor in node.neighbors:
+                new_node.neighbors.append(dfs(neighbor))
+            
+            return new_node
         
-        # Create the new node and add the old node to visited
-        new_node = Node(node.val)
-        self.visited[node] = new_node
-
-        # Run through the neighbors, and call cloneGraph for each neighbor
-        for neighbor in node.neighbors:
-            new_node.neighbors.append(self.cloneGraph(neighbor))
-        
-        # return the new_node 
-        return new_node
+        return dfs(node)
 
 
         
