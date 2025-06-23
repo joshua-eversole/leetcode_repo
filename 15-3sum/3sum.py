1e508909956fe768fe4293ada63948482aa957a2
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-06-23 13:04:10 +0800

    Time: 856 ms (21.94%) | Memory: 20.3 MB (93.09%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,38 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        # Step 1: Sort the array and init variables
         nums.sort()
+        seen = set()
+        n = len(nums)
+        i, j, k = 0, 1, 2
 
-
-        for i, num in enumerate(nums):
-            # Skipping any duplicate numbers to speed up time
+        # Step 2: Go through each i value (to n-2)
+        while i < (n-2):
+            # Check for duplicate i's
             if i > 0 and nums[i] == nums[i-1]:
-                #print(f"continuing")
+                i += 1
                 continue
-            
-            j = i + 1
-            k = len(nums) - 1
-
-            # Do a two pointer and get it closer
+            two_sum = nums[i] * -1
+            # Step 3: Do a 2-pointer search for j and k
+            j, k = i + 1, n - 1
             while j < k:
-                tri_sum = nums[i] + nums[j] + nums[k]
-                #print(f"{nums[i]} + {nums[j]} + {nums[k]} = {tri_sum}")
-                # Since it's sorted, we can just use a normal tow pointer and hone in on the solution
-                if tri_sum > 0:
-                    k -= 1
-                elif tri_sum < 0:
-                    j += 1
-                else:
-                    #If it's 0, check to make sure we haven't already added it
-                    result.append([nums[i], nums[j], nums[k]])
+                if nums[j] + nums[k] == two_sum and (nums[i], nums[j], nums[k]) not in seen:
+                    seen.add((nums[i], nums[j], nums[k]))
+                    # Move j and k so we don't get this anymore
                     j += 1
                     k -= 1
-
-                    while nums[j] == nums[j-1] and j < k:
+                elif  nums[j] + nums[k] < two_sum:
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:
                         j += 1
-
-        return result
-            
+                else:
+                    k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+        return list(seen)
 
 
 
