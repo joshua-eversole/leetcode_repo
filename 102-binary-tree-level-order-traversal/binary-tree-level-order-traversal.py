37e7229da6a23a469ec00e9751da0b3495786d9b
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-06-30 16:50:36 +0800

    Time: 1 ms (29.99%) | Memory: 18.4 MB (86.63%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -7,28 +7,51 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         # First thoughts: this can be done either bfs or dfs, but bfs is probably the better move
-        q = deque()
+
         result = []
         # Base case: if there's no root
         if not root:
             return []
+
+        def bfs():
+            q = deque()
+            
+            q.append(root)
+
+            while q:
+                row = []
+                # Inner while q is to get everything in the existing row out 
+                for _ in range(len(q)):
+                    node = q.popleft()
+
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+
+                    row.append(node.val)
+                result.append(row)
         
-        q.append(root)
+            return result
 
-        while q:
-            row = []
-            # Inner while q is to get everything in the existing row out 
-            for _ in range(len(q)):
-                node = q.popleft()
+        def dfs(node, level):
+            if not node:
+                return
 
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+            if len(result) < (level + 1):
+                result.append([node.val])
+            else:
+                result[level].append(node.val)
+            
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+            return
 
-                row.append(node.val)
-            result.append(row)
-        
+
+
+
+        #return bfs()
+        dfs(root, 0)
         return result
                 
 
