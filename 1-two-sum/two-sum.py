99c8261cec56e8600668d120bfb8ae01df78a92e
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-07-07 17:55:42 +0800

    Time: 1 ms (58.49%) | Memory: 19.4 MB (5.87%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,13 @@
+
 class Solution:
-    #11/29: pretty simple, need to reread the question because i forgot about not using the same number twice
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    hash = {}
-    for i, num in enumerate(nums):
-        hash[target - num] = i
-    for i, num in enumerate(nums):
-        if num in hash and i != hash[num]:
-            return [i, hash[num]]
-    
-    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indeces = {}
+        for i, num in enumerate(nums):
+            indeces[target - num] = i
+        
+        for i, num in enumerate(nums):
+            if num in indeces and i != indeces[num]:
+                return [i, indeces[num]]
+
+        
+                
