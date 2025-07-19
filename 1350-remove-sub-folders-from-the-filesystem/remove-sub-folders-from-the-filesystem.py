045cdde299af916f5a71cd9a6d9fbd9ced2a6cb4
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-07-19 10:18:36 +0800

    Time: 55 ms (61.78%) | Memory: 30.6 MB (77.01%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # Make a set of folder for quicker reads
+        folder_set = set(folder)
+        result = []
+
+        # Run through each directory in the folder
+        for directory in folder:
+            # Add each directory to the result
+            result.append(directory)
+            # Run through the characters and check to see if substrings are in the set
+            last_slash = 0
+            for i, char in enumerate(directory):
+                if char == '/':
+                    # If it exists in the set, then 
+                    if directory[:i] in folder_set:
+                        result.pop()
+                        break
+        
+        return result
+
+
+        
