e0650ff120c9989ac798a9725d4e0ec3ab8255c5
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-07-27 17:11:57 +0800

    Time: 43 ms (31.57%) | Memory: 18 MB (16.95%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,20 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        negative = 1
-        if x < 0: 
-            x *= -1
-            negative = -1
-        result = 0
-
-        i = len(str(x)) - 1
-        while i >= 0:
+        is_negative = True if x < 0 else False
+        x = abs(x)
+        reversed_num = 0
+        max_num = (2**31 - 1)//10
+        while x > 0:
             digit = x % 10
             x = x//10
-            result = result * 10 + digit
-            i -= 1
-        if result >= 2**31:
-            return 0
-        return result * negative
-
-
+            # make sure we don't go above the 32-bit int cap before multiplying
+            if reversed_num > max_num:
+                return 0
+            
+            reversed_num = reversed_num * 10 + digit
         
+        if is_negative:
+            return reversed_num * -1
+        else:
+            return reversed_num
+
