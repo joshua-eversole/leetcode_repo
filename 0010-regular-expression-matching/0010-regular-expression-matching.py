59b102aae3d634cdcfd51d86a37280ce485521b7
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-04 16:03:12 +0800

    Time: 6 ms (77.04%), Space: 18.3 MB (8.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,8 +2,11 @@ class Solution:
     # First thoughts: . seems self-explanatory, the tough part is when to do *. My thought is to use dynamic programming and create a new run of the function every time to see
     def isMatch(self, s: str, p: str) -> bool:
         
-
+        cache = {}
         def dfs(i, j):
+            # Base case: if we did this already
+            if (i,j) in cache:
+                return cache[(i,j)]
             # Base case
             if i >= len(s) and j >= len(p):
                 return True
@@ -14,12 +17,14 @@ class Solution:
 
             if j < len(p)-1 and p[j+1] == '*':
                 # We either skip past the star and the previous number or we move i one to the right (this can continue as many times as we want)
-                return dfs(i, j+2) or (match and dfs(i+1, j))
+                cache[(i,j)] = dfs(i, j+2) or (match and dfs(i+1, j))
+                return cache[(i,j)]
             
             if match:
-                return dfs(i+1, j+1)
-            
-            return False
+                cache[(i,j)] = dfs(i+1, j+1)
+            else:
+                cache[(i,j)] = False
+            return cache[(i,j)]
 
 
         return dfs(0, 0)
