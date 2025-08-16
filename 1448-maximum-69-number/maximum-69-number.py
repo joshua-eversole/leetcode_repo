7964b546917eb93ec9dd335dc4df428b22ba0a43
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-16 15:46:44 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.05%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         # First thoughts, num is only positive, so just replace the first 6 we see with a 9
-        num_str = str(num)
-        result = ""
-        seen_six = False
-        for i, digit in enumerate(num_str):
-            if not seen_six and digit == '6':
-                result += '9'
-                seen_six = True
-            else:
-                result += digit 
+        num_str = str(num) # Convert num to a string (so we can look at each digit individually)
+        result = "" # Make a new empty string
+        seen_six = False # Make a new true/false value to keep track of if we've already changed a six to a nine
+        for i, digit in enumerate(num_str): # Go through each digit in the string (i is the order of the digit, digit is the actual number (in string form))
+            if not seen_six and digit == '6': # If the number is a six and we haven't changed a six yet
+                result += '9' # Add a 9 instead of the 6 digit
+                seen_six = True # set seen_six to true so we don't do this multiple times
+            else: #if the number isn't a six or if we have already changed a 6
+                result += digit # just add the normal digit
         
-        return int(result)
+        return int(result) #Now convert the result back to a digit and return it
