fb9ce041f8813d8e6572ea37666d0744470ba092
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-16 16:55:02 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.80%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,24 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        def popIn(merged, list_node):
-            merged.next = list_node
-            list_node = list_node.next
-            merged = merged.next
-            return merged, list_node
-        pointer = ListNode(0)
-        result = pointer
+        def add(path, node):
+            path.next = node
+            path = path.next
+            node = node.next
+            return path, node
+        
+        result = ListNode(0)
+        dummy = result
+
         while list1 and list2:
             if list1.val < list2.val:
-                pointer, list1 = popIn(pointer, list1)
+                result, list1 = add(result, list1)
             else:
-                pointer, list2 = popIn(pointer, list2)
-        while list1:
-                pointer, list1 = popIn(pointer, list1)
-        while list2:
-                pointer, list2 = popIn(pointer, list2)
-        return result.next
-            
-
+                result, list2 = add(result, list2)
         
-                
+        while list1:
+            result, list1 = add(result, list1)
+        
+        while list2:
+            result, list2 = add(result, list2)
+        
+        return dummy.next
+
