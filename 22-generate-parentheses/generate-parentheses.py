9dce591911910316c8c457ba254ea50b997dbb0e
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-16 17:09:22 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,18 +3,18 @@ class Solution:
         # First thoughts: dfs, either add ( or add )
         result = []
         # Input: string (current parentheses combo), depth (how many parentheses deep we are)
-        def dfs(string, open, close):
+        def dfs(string, open_par, close_par):
             # Base case: if we've reached the end
             if len(string) == n*2:
                 result.append(string)
                 return
             
             # If we can still add a (
-            if open < n:
-                dfs(string + '(', open + 1, close)
+            if open_par < n:
+                dfs(string + '(', open_par + 1, close_par)
             # If depth isn't 0, add a )
-            if open > close:
-                dfs(string + ')', open, close + 1) 
+            if open_par > close_par:
+                dfs(string + ')', open_par, close_par + 1) 
         
         dfs("", 0, 0)
 
