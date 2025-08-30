1d3b9d9909c6559c38244e418d95e559fad544ce
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-30 19:15:22 +0800

    Time: 10 ms (60.95%) | Memory: 17.9 MB (39.83%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # first thoughts: dfs to scan each section of candidates, add it to the set
-        result = []
+        # this should be dynamic programming, think about the tree structure and remember it can be used an unlimited number of times (use memoization)
+        memo = {}
+        results = []
+        candidates.sort()
 
-        # Input: current path, num to hit
-        def dfs(start, path, amt_left):
-            # If we're at 0, add it to summed (if it's not there already)
-            if amt_left == 0 and path not in result:
-                result.append(path)
-            # If we havent' passed 0 yet, then continue adding nums
-            elif amt_left > 0:
-                for i in range(start, len(candidates)):
-                    print(f"i: {i}")
-                    dfs(i, path + [candidates[i]], amt_left - candidates[i])
-            # If we've already passed 0, do nothing
-        
-        dfs(0, [], target)
-        return result
+        def dp(i, amt, path):
+            # Base case
+            if amt > target:
+                return
+            elif amt == target:
+                path.sort()
+                results.append(list(path))
+            else:
+                for j, cand in enumerate(candidates):
+                    if j < i:
+                        continue
+                    path.append(cand)
+                    dp(j, amt + cand, path)
+                    path.pop()
+
+    
+        dp(0, 0, [])
+        return results
