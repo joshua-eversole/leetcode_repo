db1d473c1fed0bc0954f1977aff220233bf79e0c
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-08-30 17:03:39 +0800

    Time: 3 ms (81.81%) | Memory: 18 MB (24.98%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -6,22 +6,12 @@ class Solution:
         for r in range(9):
             for c in range(9):
                 cell = board[r][c]
-                if cell == "." or cell == ",":
-                    continue
-                if cell in rows[r]:
-                    return False
-                else:
+                box_index = r//3*3 + c // 3
+                if cell != ".":
+                    if cell in rows[r] or cell in cols[c] or cell in subboxes[box_index]:
+                        return False
                     rows[r].add(cell)
-                if cell in cols[c]:
-                    return False
-                else:
                     cols[c].add(cell)
-                #for the subboxes, i'm going left to right then up to down, so board[4][7] would go in the 6th box. i'm going to acheive this by dividing by 3 and multiplying the row amount by 3
-                subbox_index = (r//3)*3 + (c//3)
-                if cell in subboxes[subbox_index]:
-                    return False
-                else:
-                    subboxes[subbox_index].add(cell)
-
-
+                    subboxes[box_index].add(cell)
+        
         return True
