b45a5c71bfdf3788200c745dcd10301124db329a
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-09-09 14:29:02 +0800

    Time: 15 ms (51.45%) | Memory: 19.4 MB (38.81%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,24 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # Two-pointer approach, 
         l, r = 0, len(height) - 1
         l_max, r_max = height[l], height[r]
-        trapped_water = 0
-
+        water_trapped = 0
         while l < r:
-            # Move the smallest one in
+            # Move one of the pointers closer to the middle
             if height[l] < height[r]:
                 l += 1
                 if height[l] < l_max:
-                    trapped_water += (l_max - height[l])
-                else:
-                    l_max = height[l]
+                    water_trapped += (l_max - height[l])
+                l_max = max(l_max, height[l])   
+
             else:
                 r -= 1
                 if height[r] < r_max:
-                    trapped_water += (r_max - height[r])
-                else:
-                    r_max = height[r]
+                    water_trapped += (r_max - height[r])
+                r_max = max(r_max, height[r])
 
-        return trapped_water
+        return water_trapped
+            
             
         
