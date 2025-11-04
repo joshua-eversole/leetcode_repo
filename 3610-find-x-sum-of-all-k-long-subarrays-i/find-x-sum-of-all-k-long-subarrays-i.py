e91c1a6246009a5d08fdefea71b272d0ddb8a6e7
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-11-04 14:49:45 +0800

    Time: 21 ms (81.56%) | Memory: 18 MB (38.70%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,45 +1,36 @@
+from collections import defaultdict
+from typing import List
+import heapq
+
 class Solution:
+    # after getting it with sliding window, i got AI help for understanding the heapq solution. return to this in a few days
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        # Find the x most frequent elements
         num_cnt = defaultdict(int)
         res = []
 
-        # Purpose: do the x_sum calculations and return the sum of the x most frequent values
         def x_sum(window):
-            # i didn't do this correctly, i couldn't understand the dict to pairs
-            freq = sorted([(count, num) for num, count in window.items()], reverse=True)
+            # Sort by frequency descending, then value descending
+            freq = sorted(window.items(), key=lambda item: (-item[1], -item[0]))
             freq_sum = 0
-            for i, num in freq[:x]:
-                if i == 0:
-                    break
-                freq_sum += num*i
-            
+            for num, freq in freq[:x]:
+                freq_sum += num * freq
             return freq_sum
 
 
-        # Get the initial num_cnt and window_sum for res[0]
+        # Initialize first window
         for i in range(k):
             num_cnt[nums[i]] += 1
-        # Add the first result
         res.append(x_sum(num_cnt))
 
-
-        # Now go through the rest of the array and use sliding window to keep the num_cnt accurate
+        # Slide the window
         for i in range(k, len(nums)):
-            # remove the hanging num
-            remove = nums[i-k]
+            remove = nums[i - k]
             num_cnt[remove] -= 1
-            # remove 0-count values
             if num_cnt[remove] == 0:
                 del num_cnt[remove]
 
-            # add the new num
             add = nums[i]
             num_cnt[add] += 1
             res.append(x_sum(num_cnt))
+
         return res
-
-        
-
-
-
