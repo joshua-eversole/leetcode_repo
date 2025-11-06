294713ea6255af5fb37748e9e52f0b2dc7956aa8
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-11-06 13:35:17 +0800

    Time: 216 ms (91.84%) | Memory: 17.8 MB (58.79%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,36 +1,30 @@
 class Solution:
+    # Remember to do both odd and even palindromes
     def longestPalindrome(self, s: str) -> str:
-        # s will always be at least 1, so worst case this is our palindrome
-        result = s[0]
-        self.res_len = 1
-        #Input: starting l, r, and the string
-        # Output: the longest palindrome we can make with that start
-        def expandPalindrome(l,r,s):
-            palindrome = ""
-            n = len(s)
-            # While we can expland and the expansion works, expand
+        res = s[0]
+        n = len(s)
+
+        def oddPalindrome(i):
+            l, r = i - 1, i + 1
             while l >= 0 and r < n and s[l] == s[r]:
-                palindrome = s[l:r+1]
                 l -= 1
                 r += 1
-            # Once it can't work for either reason, return what we have
-            if len(palindrome) > self.res_len:
-                self.res_len = len(palindrome)
-                return palindrome
-            return result
-                
+            return s[l+1:r]
+        
+        def evenPalindrome(i, j):
+            l, r = i, j
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        
+        for i in range(n-1):
+            odd_pal = oddPalindrome(i)
+            even_pal = evenPalindrome(i,i+1)
+            if len(odd_pal) > len(res): res = odd_pal
+            if len(even_pal) > len(res): res = even_pal
 
-        # Let's avoid using the last since it'll never be bigger than s[0] anyways
-        # We want to keep i=0 in case the longest palindrome is s[0:1]
-        for i in range(len(s) - 1):
-            # Odd: l and r start at the same, let's put in l for both
-            result = expandPalindrome(i, i, s)
-            # Even: l and r start right after each other
-            result = expandPalindrome(i, i+1, s)
-
-
-        return result
-
+        return res
 
 
 
