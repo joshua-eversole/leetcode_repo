08001ee2a9ad2c7befb0751089284bba62f312a3
Joshua Eversole <47643068+joshua-eversole@users.noreply.github.com>
2025-12-30 07:39:40 +0800

    Time: 11 ms (17.64%) | Memory: 18.1 MB (94.56%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,6 +1,12 @@
 class Solution:
     def minimumTotal(self, tri: List[List[int]]) -> int:
-        for i in range(len(tri) - 2, -1, -1):
-            for j in range(len(tri[i])):
-                tri[i][j] += min(tri[i + 1][j], tri[i + 1][j + 1])
-        return tri[0][0]
+        for r in range(1, len(tri)):
+            for c in range(len(tri[r])):
+                left, right = float('inf'), float('inf')
+                if c - 1 >= 0:
+                    left = tri[r-1][c-1] 
+                if c < len(tri[r-1]):
+                    right = tri[r-1][c]
+                tri[r][c] += min(left,right)
+        
+        return min(tri[-1])
